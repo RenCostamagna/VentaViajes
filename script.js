9c34f5e0cdd9605a7c344cc0a00c1cf87081497b
RenCostamagna <ren.costamagna@gmail.com>
2024-04-26 10:15:37 -0300

    Trabajo terminado

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
+import {barcelona, roma, londres, paris} from "./ciudades.js"
 
+//Obtengo elementos del DOM
+let enlaces = document.querySelectorAll("a")
+let tituloElemento = document.getElementById("titulo")
+let subTituloElemento = document.getElementById("subtitulo")
+let parrafoElemento = document.getElementById("parrafo")
+
+enlaces.forEach(function(enlace){
+    enlace.addEventListener("click", function(){
+        enlaces.forEach(function(enlace){
+            enlace.classList.remove("active")
+        })
+        this.classList.add("active")
+
+        let contenido = obtenerContenido(this.textContent)
+
+        tituloElemento.innerHTML = contenido.titulo
+        subTituloElemento.innerHTML = contenido.subtitulo
+        parrafoElemento.innerHTML = contenido.parrafo
+    })
+
+}
+
+
+)
+
+function obtenerContenido(enlace){
+    let contenido = {
+        "Barcelona" : barcelona,
+        "Roma" : roma,
+        "Paris" : paris,
+        "Londres" : londres
+        }   
+    return contenido[enlace];
+}
